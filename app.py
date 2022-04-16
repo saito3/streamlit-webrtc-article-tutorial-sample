5b3d7a3c6ff21011e8a8099a267dfdb0d52114e5
saito3 <saito3a@icloud.com>
2022-04-17 02:00:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ st.title("My first Streamlit app")
 st.write("Hello, world")
 
 
+def process(img, size, bias):
+  # binarize
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                 cv2.THRESH_BINARY_INV, size, bias)
+  # fill holes
+  contour, hier = cv2.findContours(bw, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+  for cnt in contour:
+    cv2.drawContours(bw, [cnt], 0, 255, -1)
+  return bw
+
 class VideoProcessor:
     def __init__(self) -> None:
         self.threshold1 = 100
@@ -15,7 +26,8 @@ class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
-        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
+        img = process(img, self.threshold1, self.threshold2)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
@@ -28,5 +40,5 @@ ctx = webrtc_streamer(
     }
 )
 if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=1, max_value=101, step=2, value=51)
+    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=15)
